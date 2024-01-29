4697306c4584a4596832eb393b653c1ba1e4c09b
Kamlesh Bhatt <kb55262555@gmail.com>
2024-01-30 00:56:19 +0530

    Started Working on the app

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,39 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View className='flex justify-center items-center m-auto'>
-      <Text className='text-red-700 '>HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text className='text-red-700 text-3xl'>
+        {/* Header */}
+
+        <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+          <Image
+            source={{
+              uri: "https://links.papareact.com/wru",
+            }}
+            className='h-7 w-7 bg-gray-300 p-4 rounded-full'
+          />
+
+          <View>
+            <Text className='font-bold text-gray-400 text-xs'>
+              Deliver Now!
+            </Text>
+            <Text className='font-bold text-xl'>Current Location</Text>
+          </View>
+        </View>
+      </Text>
+    </SafeAreaView>
   );
 };
 
